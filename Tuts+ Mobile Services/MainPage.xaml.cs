0d415a27560aa4d62cc1cc8da95c7b7838f8af16
maskaravivek <maskaravivek@hotmail.com>
2015-04-07 11:18:59 +0530

    initial commit

---
+++ Tuts+ Mobile Services/MainPage.xaml.cs
@@ -0,0 +1,140 @@
+﻿using Microsoft.WindowsAzure.MobileServices;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Security.Credentials;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=391641
+
+namespace Tuts__Mobile_Services
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        MobileServiceUser user;
+        private IMobileServiceTable<UsersTable> peopleTable = App.MobileService.GetTable<UsersTable>();
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            this.NavigationCacheMode = NavigationCacheMode.Required;
+        }
+
+        /// <summary>
+        /// Invoked when this page is about to be displayed in a Frame.
+        /// </summary>
+        /// <param name="e">Event data that describes how this page was reached.
+        /// This parameter is typically used to configure the page.</param>
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            // TODO: Prepare page for display here.
+
+            // TODO: If your application contains multiple pages, ensure that you are
+            // handling the hardware Back button by registering for the
+            // Windows.Phone.UI.Input.HardwareButtons.BackPressed event.
+            // If you are using the NavigationHelper provided by some templates,
+            // this event is handled for you.
+        }
+
+        private async void ButtonLogin_Click(object sender, RoutedEventArgs e)
+        {
+            // Login the user and then load data from the mobile service.
+            await AuthenticateAsync("MicrosoftAccount");
+            await InsertUser(new UsersTable
+            {
+                ProfilePicture= string.Empty
+            });
+            // Hide the login button and load items from the mobile service.
+            this.ButtonLogin.Visibility = Windows.UI.Xaml.Visibility.Collapsed;
+        }
+
+        private async Task InsertUser(UsersTable user)
+        {
+            await peopleTable.InsertAsync(user);
+        }
+
+        private async System.Threading.Tasks.Task AuthenticateAsync(String provider)
+        {
+            string message;
+
+            // Use the PasswordVault to securely store and access credentials.
+            PasswordVault vault = new PasswordVault();
+            PasswordCredential credential = null;
+
+            while (credential == null)
+            {
+                try
+                {
+                    // Try to get an existing credential from the vault.
+                    credential = vault.FindAllByResource(provider).FirstOrDefault();
+                }
+                catch (Exception)
+                {
+                    // When there is no matching resource an error occurs, which we ignore.
+                }
+
+                if (credential != null)
+                {
+                    // Create a user from the stored credentials.
+                    user = new MobileServiceUser(credential.UserName);
+                    credential.RetrievePassword();
+                    user.MobileServiceAuthenticationToken = credential.Password;
+
+                    // Set the user from the stored credentials.
+                    App.MobileService.CurrentUser = user;
+
+                    try
+                    {
+                        // Try to return an item now to determine if the cached credential has expired.
+                        await App.MobileService.GetTable<UsersTable>().Take(1).ToListAsync();
+                    }
+                    catch (MobileServiceInvalidOperationException ex)
+                    {
+                        if (ex.Response.StatusCode == System.Net.HttpStatusCode.Unauthorized)
+                        {
+                            // Remove the credential with the expired token.
+                            vault.Remove(credential);
+                            credential = null;
+                            continue;
+                        }
+                    }
+                }
+                else
+                {
+                    try
+                    {
+                        // Login with the identity provider.
+                        user = await App.MobileService.LoginAsync(provider);
+
+                        // Create and store the user credentials.
+                        credential = new PasswordCredential(provider, user.UserId, user.MobileServiceAuthenticationToken);
+                        vault.Add(credential);
+                    }
+                    catch (MobileServiceInvalidOperationException ex)
+                    {
+                        message = "You must log in. Login Required";
+                    }
+                }
+                message = string.Format("You are now logged in - {0}", user.UserId);
+                var dialog = new MessageDialog(message);
+                dialog.Commands.Add(new UICommand("OK"));
+                await dialog.ShowAsync();
+            }
+        }
+    }
+}
---
+++ Tuts+ Mobile Services/UsersTable.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tuts__Mobile_Services
+{
+    class UsersTable
+    {
+        [JsonProperty(PropertyName = "id")]
+        public string Id { get; set; }
+
+        [JsonProperty(PropertyName = "userId")]
+        public string UserID { get; set; }
+
+        [JsonProperty(PropertyName = "user_email")]
+        public string Email { get; set; }
+
+        [JsonProperty(PropertyName = "profile_picture")]
+        public string ProfilePicture { get; set; }
+
+        [JsonProperty(PropertyName = "display_name")]
+        public string DisplayName { get; set; }
+
+    }
+}
